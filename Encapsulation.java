1855b85432270565b84f152ca4571dc622b076b8
roymutua <roy.ngure@gmail.com>
2026-09-22 10:39:54 +0300

    String_Constructor

--- Encapsulation.java
+++ Encapsulation.java
@@ -36,7 +36,6 @@ public class Encapsulation
         System.out.println(obj.getName() + " : " + obj.getAge());
     }
 }
-*/
 
 class People_get
 {
@@ -70,5 +69,82 @@ public class Encapsulation
         obj.setAge(30);
 
         System.out.println(obj.getName() + " : " + obj.getAge());
+    }
+}
+
+//THIS KEYWORD IN JAVA
+class People_get
+{
+    private String name;
+    private int age;
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Encapsulation
+{
+    public static void main() {
+        People_get obj = new People_get();
+        obj.setName("Java");
+        obj.setAge(30);
+
+        System.out.println(obj.getName() + " : " + obj.getAge());
+    }
+}
+ */
+
+//CONSTRUCTOR (Default vs Parameterized)
+class People_get
+{
+    private String name;
+    private int age;
+
+    //Constructor
+    public People_get() //Default Constructor
+    {
+        System.out.println("This is a default constructor.....  ");
+        name = "Raddy";
+        age = 28;
+    }
+    public People_get(String i, int j) //Parameterized Constructor
+    {
+        System.out.println("This is a parameterized constructor.....  ");
+        name = i;
+        age = j;
+    }
+
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
+public class Encapsulation
+{
+    public static void main() {
+        People_get obj = new People_get();
+        People_get obj1 = new People_get("John", 35);
+
+        System.out.println(obj.getName() + " : " + obj.getAge());
+        System.out.println(obj1.getName() + " : " + obj1.getAge());
     }
 }
